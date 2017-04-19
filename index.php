944be2d1b64eee16d7aa7c3e9120a2f86cb79395
Chris Soueidan <chris@loogart.com>
2017-04-19 14:43:29 -0400

    first commit all files

---
+++ index.php
@@ -0,0 +1,176 @@
+<?php include 'head.php';?>
+<html>
+
+<body>
+
+    <section class="super-body">
+
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                    <a class="navbar-brand logo" href="#"></a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tradeshows <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">90 Elgin</a></li>
+                                <li><a href="#">Sparks Street</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Programme <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Presentations</a></li>
+                                <li><a href="#">Regions' Zone</a></li>
+                                <li><a href="#">FED Talks</a></li>
+                                <li><a href="#">Workshops</a></li>
+                                <li><a href="#">Health and Awareness Zone</a></li>
+                                <li><a href="#">Recruitment Room</a></li>
+                                <li><a href="#">Living Library</a></li>
+                                <li><a href="#">Blueprint TV</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="#about">Virtual Kit</a></li>
+                        <li><a href="#contact">Connnect</a></li>
+                        <li><a href="#contact">FR</a></li>
+                        <li><a href="http://www.gcpedia.gc.ca/gcwiki/index.php?title=Innofair&action=edit">ª</a></li>
+
+                    </ul>
+                </div>
+                <!--/.nav-collapse -->
+            </div>
+        </nav>
+
+        <!-- Jumbotron -->
+        <div class="jumbotron j1">
+            <div class="container">
+                <h1>2017 Innovation Fair</h1>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <p style="letter-spacing:1px;">In tune with the Canada 150 celebrations, the Fair also offers a unique opportunity to showcase the Public Service’s own 150th anniversary and to highlight the results and outcomes we have achieved over the years for Canada and Canadians.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- content -->
+        <section class="home-tiles">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-blue-1">
+                                <img src="images/">
+                                <h2>Tradeshows</h2>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-blue-main">
+                                <img src="images/">
+                                <h2>Regions' Zone</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-blue-main">
+                                <img src="images/">
+                                <h2>FED Talks</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-green-1">
+                                <img src="images/">
+                                <h2>Workshops</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-blue-2">
+                                <img src="images/">
+                                <h2>Health</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-blue-main">
+                                <img src="images/">
+                                <h2>Recruitment</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-orange-1">
+                                <img src="images/">
+                                <h2>Living Library</h2>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#">
+                            <div class="tile background-blue-3">
+                                <img src="images/">
+                                <h2>Blueprint TV</h2>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- footer -->
+        <section class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h2>News and events</h2>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4> March 14, 2017</h4>
+                        <p>Featured Building – 65′ x 96′ VP Series Fabric Structure – Manhattan Beer Distributors</p>
+                        <a href="">Read more </a>
+                    </div>
+                    <div class="col-sm-6">
+                        <h4> March 14, 2017</h4>
+                        <p>Featured Building – 65′ x 96′ VP Series Fabric Structure – Manhattan Beer Distributors</p>
+                        <a href="">Read more </a>
+                    </div>
+                    <div class="col-sm-12">
+                        <hr>
+                    </div>
+                    <div class="col-sm-4">
+                        <img class="img-responsive" src="images/logo-en.svg">
+                    </div>
+                    <div class="col-sm-8 text-right">
+                        <h6>© 2017 Designed by PCO’s friends @GAC, Chris Soueidan</h6>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </section>
+
+
+</body>
+
+</html>
+{{Innofair-styles}}
